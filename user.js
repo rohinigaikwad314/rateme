99b18e6f57af573874ba1bef589a7cc5975b551f
rohinigaikwad314 <48656396+rohinigaikwad314@users.noreply.github.com>
2019-04-22 02:15:17 +0530

    Add files via upload

--- user.js
+++ user.js
@@ -1,23 +1,21 @@
-var passport = require('passport');
-module.exports = (app)=>{
+var mongoose= require('mongoose');
+var bcrypt= require('bcrypt-nodejs');
 
-  app.get('/',(req,res,next)=>{
-     res.render('index',{
-       title: 'Index || RateMe'});
+var userSchema= mongoose.Schema({
+    fullname: {type:String,required:true},
+    email: {type: String,required: true},
+    password: {type: String},
+    role: {type: String, default: ''},
+    company: {
+      name: {type: String, default: ''},
+      image: {type:String, default:''}
+    },
+    passwordResetToken:{type: String, default: ''},
+    passwordResetExpires:{type: Date, default: Date.now}
   });
 
-  app.get('/signup',(req,res)=>{
-    res.render('user/signup',{
-      title: 'Sign Up || RateMe'});
-  });
+  userSchema.methods.encryptPassword= (password) =>{
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(10), null);
+  }
 
-  app.post('/signup',passport.authenticate('local.signup',{
-    successRedirect: '/',
-    failureRedirect: '/signup',
-    failureFlash: true
-  }));
-  app.get('/login',(req,res)=>{
-    res.render('user/login',{
-      title: 'Login || RateMe'});
-  })
-}
+  module.exports= mongoose.model('User',userSchema);
